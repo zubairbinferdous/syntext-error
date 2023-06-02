480eaaeab3de3a5a639e48a6e2cbb14012514a28
zubairbinferdous <zubidev71@gmail.com>
2023-06-02 11:23:51 +0600

    ok

---
+++ syntext.js
@@ -0,0 +1,83 @@
+function isBigger(num){
+    if(num > 5){
+        return true;
+    }
+    else {
+        return false
+    };
+}
+// } 
+
+const result = isBigger(11);
+// console.log(result);
+
+
+// const sum = a, b => a+b;
+// const sum = (a, b) => a+b;
+const res = sum(3, 5);
+
+
+const musk = {
+    name: 'elon',
+    // age: 53
+    age: 53,
+    job: 'tesla'
+}
+
+console.log(musk.age);
+
+const numbers = [12, 13, 21, 3, 34, 4, 4, ]
+
+for(let i = 0; i< 10; i++){
+    // const n = numbers[i;
+    const n = numbers[i];
+}
+
+
+console.log(numbers)
+
+// if (numbers.length > 5) && (numbers[0] === 5){
+    
+// } 
+
+if (numbers.length > 5 && numbers[0] === 5){
+    
+} 
+
+const numbs = 5;
+// numbs.push(45);
+
+// let address;
+// console.log(address.home);
+let address ={ city: 'bBaria', postCode: 4560};
+// console.log(address.home.building);
+console.log(address?.home?.building);
+
+const a = 'abul';
+a = 'babul';
+
+const xx = 'Pori'
+const y = 'moni'
+console.log(x);
+// console.log(z);
+
+// console.log(name);
+// const name = 'Ananata Jalil';
+// let name = 'Ananata Jalil';
+// var name = 'Ananata Jalil';
+
+// console.log(add2(2,3));
+
+function add(n1, n2) {
+    return n1 + n2
+}
+
+
+const add2 = (n1, n2) => n1 + n2;
+
+
+function doSomething () {
+    const sum = 55;
+}
+
+// console.log(sum);
